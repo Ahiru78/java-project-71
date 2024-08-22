ad844adbdc7f427cea33c73b2b952ca9ff1d1867
Ahiru78 <bor.limatov@gmail.com>
2024-08-23 01:42:15 +0300

    Add preps

---
+++ app/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    id("java")
+    application
+    checkstyle
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("info.picocli:picocli:4.7.6")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.0.1")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+application {
+    mainClass = "hexlet.code.App"
+}
+
+tasks.getByName("run", JavaExec::class) {
+    standardInput = System.`in`
+}
---
+++ app/src/main/java/hexlet/code/Parse.java
@@ -0,0 +1,9 @@
+package hexlet.code;
+
+import java.util.Map;
+
+public class Parse {
+    public static Map getData(String content) throws Exception {
+        return parse(content);
+    }
+}
